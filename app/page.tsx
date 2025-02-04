15c379af68e7d94f5757b4749ff4f995134f87f1
yashin4112 <yash.shinde@contentstack.com>
2025-02-04 20:38:12 +0530

    force to fetch new data in page.tsx by adding force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Home from '../components/home'; // Import the Home component
 import { getAllEntries } from '@/helper'; // Import the getAllEntries function
 
+export const dynamic = "force-dynamic"; 
+
 export default async function Page() {
   // Fetch data from getAllEntries
   const homePageData = await getAllEntries();
-  // const response = await uploadAssetToContentstack();
-  // console.log("res",response)
 
-  // Pass the fetched data as a prop to the Home component
   return (
     <div>
       <Home homePageData={homePageData} /> {/* Passing the data as a prop */}
